bdd4ec86ba1623a4af3e7fc6e1eefedccb749294
Paolo Leopardi <leopardipaolo@gmail.com>
2020-04-24 09:39:21 +0200

    chore(store): add logger

--- src/presentation/store/store.ts
+++ src/presentation/store/store.ts
@@ -1,12 +1,21 @@
-import { combineReducers, createStore } from 'redux';
-import { reducer as positionReducer } from './position/reducers';
-import { reducer as weatherReducer } from './weather/reducers';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+import { PositionState, reducer as positionReducer } from './position';
+import { reducer as weatherReducer, WeatherState } from './weather';
+
+export interface State {
+  position: PositionState;
+  weather: WeatherState;
+}
 
 const rootReducer = {
   position: positionReducer,
   weather: weatherReducer,
 };
 
-export const store = createStore(combineReducers(rootReducer));
+export const store = createStore(
+  combineReducers(rootReducer),
+  applyMiddleware(logger)
+);
 
 export const dispatch = store.dispatch.bind(store);
