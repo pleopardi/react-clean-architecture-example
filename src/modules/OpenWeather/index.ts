927073f4521f199defb09ff2e88f80aa4795a87e
Paolo Leopardi <leopardipaolo@gmail.com>
2020-04-25 17:21:35 +0200

    chore(OpenWeather): add API key fallback

--- src/modules/OpenWeather/index.ts
+++ src/modules/OpenWeather/index.ts
@@ -1,7 +1,7 @@
 import { OpenWeather } from './OpenWeather';
 
 export function makeOpenWeatherInstance() {
-  return new OpenWeather(process.env.OPEN_WEATHER_API_KEY as string);
+  return new OpenWeather(process.env.OPEN_WEATHER_API_KEY ?? '');
 }
 export { OpenWeather } from './OpenWeather';
 export { WeatherResponse } from './types';
