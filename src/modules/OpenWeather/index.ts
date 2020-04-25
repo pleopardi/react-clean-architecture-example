5ff5428f8ca4516d23d5fa644c6eb0d1c03bc259
Paolo Leopardi <leopardipaolo@gmail.com>
2020-04-25 17:10:48 +0200

    feat(OpenWeather): use env variable

--- src/modules/OpenWeather/index.ts
+++ src/modules/OpenWeather/index.ts
@@ -1,9 +1,7 @@
 import { OpenWeather } from './OpenWeather';
 
-const API_KEY = 'ac05c2db239c8c0a196330ddd81febe9';
-
-export function makeOpenWeatherInstance(key: string = API_KEY) {
-  return new OpenWeather(key);
+export function makeOpenWeatherInstance() {
+  return new OpenWeather(process.env.OPEN_WEATHER_API_KEY as string);
 }
 export { OpenWeather } from './OpenWeather';
 export { WeatherResponse } from './types';
